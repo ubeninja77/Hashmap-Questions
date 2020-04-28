24f7202dbab6e0386104e1293d9364d5a8129a4c
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-28 10:58:23 -0700

    added permutations method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,25 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+
+  hash = {}
+  if string1.length != string2.length
+    return false
+  end
+
+  string1.each_char do |letter|
+    if hash[letter]
+      hash[letter] += 1
+    else
+      hash[letter] = 1
+    end
+  end
+
+  string2.each_char do |letter|
+    if hash[letter] && hash[letter] != 0 
+      hash[letter] -= 1
+    else
+      return false
+    end
+  end
+  return true
 end
