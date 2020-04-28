7182ba7eaf46fd7ffa83eee8a7e94b2f36b2f7f7
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-28 10:54:36 -0700

    added palindrome method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,32 @@
+# Write a method which will take two strings as arguments 
+# and returns true if one string is a permutation of the other.
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  hash = {}
+  count = 0
+
+  if string.empty?
+    return true
+  end
+
+  string.each_char do |i|
+    if hash[i] == nil
+      hash[i] = 1
+    else
+      hash[i] += 1
+    end
+  end
+
+  string_array = hash.values
+  string_array.each do |num|
+    if num.odd? == true
+      count += 1
+      if count > 1
+        return false
+      end
+    end
+  end
+  return true
+
+
 end
